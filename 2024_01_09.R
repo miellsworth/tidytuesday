4490e33301bcc94476375528b286a15ca9b7a5a6
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-03-18 07:33:51 -0600

    new df monthly proportions

--- 2024_01_09.R
+++ 2024_01_09.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(tidytuesdayR)
 library(here)
 library(stringr)
+library(lubridate)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -12,14 +13,33 @@ last_tues <- "2024-01-09"
 
 # Creating directory for draft plots
 plt_dir <- paste0("plt_", last_tues)
-dir.create(here("plots", "drafts", plt_dir))
+# dir.create(here("plots", "drafts", plt_dir))
 
 # Get the Data
-tuesdata <- tidytuesdayR::tt_load('2024-01-09')
+tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
+can_births <- tuesdata$canada_births_1991_2022
+nhl_births <- tuesdata$nhl_player_births
 
 # Transform data
+can_births %>% 
+  group_by(month) %>%
+  summarise(total_births = sum(births)) %>%
+  ggplot(aes(x = as.factor(month), y = total_births)) +
+  geom_col()
+
+can_monthly_births <- can_births %>% 
+  group_by(month) %>%
+  summarise(total_monthly_births = sum(births)) %>%
+  mutate(monthly_proportion = total_monthly_births/sum(total_monthly_births))
+
+nhl_monthly_births <- nhl_births %>% count(birth_month) %>%
+  mutate(monthly_proportion = n/sum(n)) %>%
+  rename(
+    total_monthly_births = n,
+    month = birth_month
+  )
 
 # Plot data
 
