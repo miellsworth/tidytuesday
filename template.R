93a8f42baa182cced206d90f27c18fb414686217
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-11-25 12:18:13 -0700

    create template file

---
+++ template.R
@@ -0,0 +1,38 @@
+# Load libraries
+library(tidytuesdayR)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(here)
+
+# Find the most recent Tuesday
+tidytuesdayR::last_tuesday()
+last_tues <- "" # Date in YYYY-MM-DD format
+
+# View README
+tt_output <- tt_load_gh(last_tues)
+readme(tt_output)
+
+# Creating directory for draft plots
+drafts <- here(year(last_tues), last_tues, "drafts")
+dir.create(drafts)
+
+# Get the Data
+tuesdata <- tidytuesdayR::tt_load(last_tues)
+
+# Tidy data
+
+
+# Plot data
+
+
+# Save draft plots
+ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
+
+# Save final
+plot_title <- "" # Include informative title
+ggsave(
+  here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
+  plot
+)
