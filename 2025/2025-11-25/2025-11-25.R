58879a4c18f218651016f1e3423ce9eccdc81112
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-11-25 12:26:49 -0700

    update 2025-11-25

--- 2025/2025-11-25/2025-11-25.R
+++ 2025/2025-11-25/2025-11-25.R
@@ -8,14 +8,34 @@ library(here)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
-last_tues <- "2025-11-25" # Date in YYYY-MM-DD format
+last_tues <- "" # Date in YYYY-MM-DD format
 
 # View README
 tt_output <- tt_load_gh(last_tues)
 readme(tt_output)
 
-# Creating directory for draft plots
-drafts <- here(year(last_tues), last_tues, "drafts")
-drafts
+# Create directories
+## Main
+dir.create(here(year(last_tues), last_tues))
 
-dir.create(drafts)
+## Drafts
+dir.create(here(year(last_tues), last_tues, "drafts"))
+
+# Get the Data
+tuesdata <- tidytuesdayR::tt_load(last_tues)
+
+# Tidy data
+
+
+# Plot data
+
+
+# Save draft plots
+ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
+
+# Save final
+plot_title <- "" # Include informative title
+ggsave(
+  here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
+  plot
+)
