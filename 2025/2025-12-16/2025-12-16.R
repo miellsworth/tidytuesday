0397386dd984ea16381cebca1ba1d04ef9a2e2f0
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-15 06:10:42 -0700

    create R script placeholder for 2025-12-16

---
+++ 2025/2025-12-16/2025-12-16.R
@@ -0,0 +1,41 @@
+# Load libraries
+library(tidytuesdayR)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(here)
+
+# Find the most recent Tuesday
+tidytuesdayR::last_tuesday()
+last_tues <- "2025-12-16" # Date in YYYY-MM-DD format
+
+# View README
+tt_output <- tt_load_gh(last_tues)
+readme(tt_output)
+
+# Create directories
+## Main
+dir.create(here(year(last_tues), last_tues))
+
+## Drafts
+dir.create(here(year(last_tues), last_tues, "drafts"))
+
+# Get the Data
+tuesdata <- tidytuesdayR::tt_load(last_tues)
+
+# Tidy data
+roundabouts <- tuesdata$roundabouts_clean
+
+# Plot data
+
+
+# Save draft plots
+ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
+
+# Save final
+plot_title <- "" # Include informative title
+ggsave(
+  here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
+  plot
+)
