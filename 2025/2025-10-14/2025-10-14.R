17d9578704ea200a345b4c777bd2ff78fb387d99
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-15 12:53:07 -0700

    create placeholder R scripts for 2025-10-07 to 2025-12-16

--- 2025/2025-10-14/2025-10-14.R
+++ 2025/2025-10-14/2025-10-14.R
@@ -8,7 +8,7 @@ library(here)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
-last_tues <- "2025-12-16" # Date in YYYY-MM-DD format
+last_tues <- "2025-10-14" # Date in YYYY-MM-DD format
 
 # View README
 tt_output <- tt_load_gh(last_tues)
@@ -25,7 +25,7 @@ dir.create(here(year(last_tues), last_tues, "drafts"))
 tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
-roundabouts <- tuesdata$roundabouts_clean
+food_security <- tuesdata$food_security
 
 # Plot data
 
@@ -34,7 +34,7 @@ roundabouts <- tuesdata$roundabouts_clean
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
 
 # Save final
-plot_title <- "roundabouts" # Include informative title
+plot_title <- "" # Include informative title
 ggsave(
   here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
   plot
--- 2025/2025-12-02/2025-12-02.R
+++ 2025/2025-12-02/2025-12-02.R
@@ -8,7 +8,7 @@ library(here)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
-last_tues <- "2025-12-16" # Date in YYYY-MM-DD format
+last_tues <- "2025-12-02" # Date in YYYY-MM-DD format
 
 # View README
 tt_output <- tt_load_gh(last_tues)
@@ -25,7 +25,7 @@ dir.create(here(year(last_tues), last_tues, "drafts"))
 tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
-roundabouts <- tuesdata$roundabouts_clean
+sechselaeuten <- tuesdata$sechselaeuten
 
 # Plot data
 
@@ -34,7 +34,7 @@ roundabouts <- tuesdata$roundabouts_clean
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
 
 # Save final
-plot_title <- "roundabouts" # Include informative title
+plot_title <- "exploding_snowman" # Include informative title
 ggsave(
   here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
   plot
