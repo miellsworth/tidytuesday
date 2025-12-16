bfce25b91219861711c7f0c5b5c300cf4a11af93
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-16 04:58:26 -0700

    first draft plot of 2025-12-16

--- 2025/2025-12-16/2025-12-16.R
+++ 2025/2025-12-16/2025-12-16.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
+library(stringr)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -26,9 +27,34 @@ tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
 roundabouts <- tuesdata$roundabouts_clean
+roundabout_types <- roundabouts |>
+  filter(year_completed != 0) |>
+  group_by(year_completed) |>
+  count(type) |>
+  ungroup() |>
+  group_by(type) |>
+  mutate(
+    running_total = cumsum(n)
+  )
 
 # Plot data
-
+plot <- roundabout_types |>
+  ggplot(aes(x = year_completed, y = running_total, col = type)) +
+  geom_line() +
+  labs(
+    title = str_wrap(""),
+    x = "",
+    caption = "Chart: Michael Ellsworth | Data: Kittelson & Associates Roundabouts Data",
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.box.margin = margin(0, 10, 0, 0),
+    legend.position = "right"
+  )
+plot
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
