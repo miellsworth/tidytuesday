973e7c1a375107f6d2602c2107bca9d4e1ac94b5
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-16 06:48:29 -0700

    first draft of 2025-12-09

--- 2025/2025-12-09/2025-12-09.R
+++ 2025/2025-12-09/2025-12-09.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
+library(stringr)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -26,9 +27,29 @@ tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
 qatarcars <- tuesdata$qatarcars
+glimpse(qatarcars)
 
 # Plot data
-
+plot <- qatarcars |>
+  filter(price < 400000) |>
+  ggplot(aes(x = price, y = performance, col = mass)) +
+  scale_x_continuous(labels = scales::number) +
+  geom_point() +
+  labs(
+    title = str_wrap("Want an expensive car in Qatar? She's going to be fast! (And light)"),
+    x = "Price (Qatari riyals)",
+    y = "Performance (seconds to 100 km/h)",
+    caption = "Chart: Michael Ellsworth | Data: Paul Musgrave's Qatar Cars",
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.box.margin = margin(0, 10, 0, 0),
+    legend.position = "right"
+  )
+plot
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
