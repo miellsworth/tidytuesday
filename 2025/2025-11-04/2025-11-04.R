7326bcd1c50aebdeda7d02e69e0c5a46199c9140
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-17 07:20:57 -0700

    first draft 2025-11-04

--- 2025/2025-11-04/2025-11-04.R
+++ 2025/2025-11-04/2025-11-04.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
+library(stringr)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -25,11 +26,39 @@ dir.create(here(year(last_tues), last_tues, "drafts"))
 tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
-flint_mdeq <- tuesdata$flint_mdeq
-flint_vt <- tuesdata$flint_vt
+flint_mdeq <- tuesdata$flint_mdeq |>
+  mutate(source = "mdeq")
+flint_vt <- tuesdata$flint_vt  |>
+  mutate(source = "vt")
+
+flint_combined <- flint_mdeq |>
+  bind_rows(flint_vt)
 
 # Plot data
+plot_multi_histogram <- function(df, feature, label_column) {
+  plt <- ggplot(df, aes(x = eval(parse(text = feature)), fill = eval(parse(text = label_column)))) +
+    geom_histogram(alpha = 0.7, position = "identity", aes(y = after_stat(density)), color="black") +
+    geom_density(alpha = 0.7) +
+    geom_vline(aes(xintercept = mean(eval(parse(text = feature)))), color = "black", linetype = "dashed", linewidth = 1) +
+    labs(x = feature, y = "Density")
+  plt + guides(fill = guide_legend(title = label_column))
+}
 
+plot_multi_histogram(flint_combined, "lead", "source")  +
+  labs(
+    title = str_wrap("Lead sample distribution comparison - MDEQ and VT"),
+    x = "Lead level (ppb)",
+    caption = "Chart: Michael Ellsworth | Data: Loux and Gibson's Flint Michigan dataset",
+  ) +
+  theme_classic() +
+  scale_x_continuous(expand = expansion(mult = c(0.01, 0))) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0))) +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.position = "right"
+  )
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
