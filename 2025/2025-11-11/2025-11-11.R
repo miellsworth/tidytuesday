656311f791b6b85af046911152560be1b568916e
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-17 05:35:07 -0700

    first draft 2025-11-11

--- 2025/2025-11-11/2025-11-11.R
+++ 2025/2025-11-11/2025-11-11.R
@@ -26,9 +26,30 @@ tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
 tuberculosis <- tuesdata$who_tb_data
+glimpse(tuberculosis)
 
 # Plot data
-
+plot <- tuberculosis |>
+  group_by(year) |>
+  summarise(c_cdr = mean(c_cdr, na.rm = TRUE) / 100) |>
+  ggplot(aes(x = year, y = c_cdr)) +
+  geom_line() +
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = str_wrap("Tuberculosis case detection rate is slowly on the rise."),
+    x = "",
+    y = "Case Detection Rate (%)",
+    caption = "Chart: Michael Ellsworth | Data: Sam Abbott's getTBinR package",
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.box.margin = margin(0, 10, 0, 0),
+    legend.position = "right"
+  )
+plot
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
