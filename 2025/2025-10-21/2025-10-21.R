ee2da951139e3e54c0ba327b13f1e2d0919ff883
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-18 06:20:21 -0700

    preliminary r script 2025-10-21

--- 2025/2025-10-21/2025-10-21.R
+++ 2025/2025-10-21/2025-10-21.R
@@ -5,6 +5,9 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
+library(stringr)
+library(forcats)
+library(patchwork)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -28,8 +31,69 @@ tuesdata <- tidytuesdayR::tt_load(last_tues)
 station <- tuesdata$historic_station_met
 station_meta <- tuesdata$station_meta
 
-# Plot data
+top_10_rain <- station |>
+  mutate(date = ym(paste(year, month)), station = str_to_title(station)) |>
+  group_by(station) |>
+  summarise(avg_rain = mean(rain, na.rm = TRUE)) |>
+  arrange(desc(avg_rain)) |>
+  head(10)
 
+top_10_tmax <- station |>
+  mutate(date = ym(paste(year, month)), station = str_to_title(station)) |>
+  group_by(station) |>
+  summarise(avg_tmax = mean(tmax, na.rm = TRUE)) |>
+  arrange(desc(avg_tmax)) |>
+  head(10)
+
+top_10_tmin <- station |>
+  mutate(date = ym(paste(year, month)), station = str_to_title(station)) |>
+  group_by(station) |>
+  summarise(avg_tmin = mean(tmin, na.rm = TRUE)) |>
+  arrange(desc(avg_tmin)) |>
+  head(10)
+
+# Plot data
+plot1 <- top_10_rain |>
+  ggplot(aes(x = fct_reorder(station, avg_rain), y = avg_rain)) +
+  geom_col(width = 0.05) +
+  geom_point(size = 12) +
+  geom_text(aes(label = round(avg_rain, 0)), color = "white") +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+  coord_flip() +
+  labs(
+    subtitle = str_wrap("Top 10 rainfall stations"),
+    x = "Weather stations",
+    y = "Average rain (mm)"
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    axis.ticks.y = element_blank()
+  )
+plot1
+
+plot1 <- top_10_rain |>
+  ggplot(aes(x = fct_reorder(station, avg_rain), y = avg_rain)) +
+  geom_col(width = 0.05) +
+  geom_point(size = 12) +
+  geom_text(aes(label = round(avg_rain, 0)), color = "white") +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+  coord_flip() +
+  labs(
+    subtitle = str_wrap("Top 10 rainfall stations"),
+    x = "Weather stations",
+    y = "Average rain (mm)"
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    axis.ticks.y = element_blank()
+  )
+plot1
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
