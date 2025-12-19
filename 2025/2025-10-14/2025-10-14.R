0ebbcc31b4d617c995dd032357f2dafeab8cba40
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-19 07:48:06 -0700

    first draft 2025-10-14

--- 2025/2025-10-14/2025-10-14.R
+++ 2025/2025-10-14/2025-10-14.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
+library(stringr)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -29,19 +30,47 @@ food_security <- tuesdata$food_security
 food_security$Item |> unique()
 
 # Plot data
-food_security |>
+plot_data <- food_security |>
   filter(
     Item == "Prevalence of severe food insecurity in the total population (percent) (3-year average)",
-    Area %in% c("Asia", "Africa", "Oceania", "South America", "Central America", "Europe", "North America")
-  ) |>
-  ggplot(aes(x = Year_End, y = Value, col = Area)) +
-  geom_line()
+    Area %in% c("Asia", "Africa", "Oceania", "South America", "Central America", "Europe", "Northern America")
+  )
+
+plot_data_point <- plot_data |>
+  filter(Year_End %in% c(2016, 2024))
+
+plot <- ggplot() +
+  geom_line(aes(x = Year_End, y = Value / 100, col = Area), data = plot_data, linewidth = 1.5) +
+  geom_point(aes(x = Year_End, y = Value / 100, col = Area), data = plot_data_point, size = 5) +
+  geom_text(aes(x = Year_End, y = Value / 100, label = Area, col = Area), data = plot_data_point |> filter(Year_End == 2024), hjust = 0, nudge_x = 0.2) +
+  scale_y_continuous(
+    labels = scales::percent,
+    expand = expansion(mult = c(0.05, 0.05))
+    ) +
+  scale_x_continuous(
+    expand = expansion(mult = c(0.05, 0.2))
+    ) +
+  scale_colour_hue(c = 40) +
+  labs(
+    title = str_wrap("Over the last 8 years, the proportion of populations experiencing food insecurty has generally increased across all continents."),
+    x = "Year",
+    y = "Prevalence (%)",
+    color = "Continent"
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.position = "none"
+  )
+plot
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
 
 # Save final
-plot_title <- "" # Include informative title
+plot_title <- "food_insecurity" # Include informative title
 ggsave(
   here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
   plot
