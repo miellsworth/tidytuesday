a99259209802ab0874ee588f17dff3080aa9722a
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-12-19 13:52:23 -0700

    first draft 2025-10-07

--- 2025/2025-10-07/2025-10-07.R
+++ 2025/2025-10-07/2025-10-07.R
@@ -5,6 +5,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(here)
+library(janitor)
+library(forcats)
 
 # Find the most recent Tuesday
 tidytuesdayR::last_tuesday()
@@ -25,16 +27,61 @@ dir.create(here(year(last_tues), last_tues, "drafts"))
 tuesdata <- tidytuesdayR::tt_load(last_tues)
 
 # Tidy data
-euroleague <- tuesdata$euroleague_basketball
+euroleague <- tuesdata$euroleague_basketball |>
+  clean_names()
+glimpse(euroleague)
 
 # Plot data
-
+plot <- euroleague |>
+  filter(final_four_appearances > 0) |>
+  ggplot(aes(x = fct_reorder(team, final_four_appearances), y = final_four_appearances)) +
+  geom_col() +
+  geom_text(
+    aes(
+      x = team, 
+      y = 0, 
+      label = team,
+      hjust = 0,
+    ),
+    col = "white",
+    nudge_y = 0.1
+  ) +
+  geom_text(
+    aes(
+      x = team, 
+      y = final_four_appearances, 
+      label = final_four_appearances,
+      hjust = 0,
+    ),
+    col = "black",
+    nudge_y = 0.1
+  ) +
+  coord_flip() +
+  theme_classic() +
+  labs(
+    title = str_wrap("Euroleague Men's Basketball - Final Four Appearances"),
+    caption = "Chart: Michael Ellsworth | Data:  EuroLeague Basketball"
+  ) +
+  theme(
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.line.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.line.x = element_blank(),
+    axis.title.x = element_blank(),
+    panel.background = element_rect(fill = "#fbfae4", color = NA),
+    plot.background = element_rect(fill = "#fbfae4", color = NA),
+    legend.background = element_rect(fill = "#fbfae4", color = NA),
+  )
+plot
 
 # Save draft plots
 ggsave(here(drafts, paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), ".png")))
 
 # Save final
-plot_title <- "" # Include informative title
+plot_title <- "euroleague" # Include informative title
 ggsave(
   here(year(last_tues), last_tues, paste0(last_tues, "_", plot_title, ".png")), 
   plot
